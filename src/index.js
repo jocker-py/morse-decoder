bcf4b7db28ee82aa083634a2c7ea52dd5978999c
jocker-py <buladzislau@gmail.com>
2022-02-24 15:48:31 +0300

    feat: add solution for Morse-decoder

--- src/index.js
+++ src/index.js
@@ -39,8 +39,39 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+
+    let str = '';
+    let expr_arr;
+    if (expr.includes('**********')){
+        expr_arr = expr.trim().split('**********')
+    } else {
+        expr_arr = [expr.trim()];
+    }
+
+    for (let i = 0; i <= expr_arr.length - 1; i++){
+        let letterFull = '';
+        let letterJust = '';
+
+        for(let j = 0; j < expr_arr[i].length; j+=10){
+            letterFull = expr_arr[i].slice(j, j+10);
+            letterJust = letterFull.slice(letterFull.indexOf('1'));
+            let key = '';
+            for(let k = 0; k < letterJust.length; k+= 2){
+                if (letterJust.slice(k, k+2) === '10'){
+                    key += '.';
+                } else {
+                    key += '-';
+                }
+            }
+            str += MORSE_TABLE[key];
+
+        }
+    str += ' ';
+    }
+    return str.trim();
 }
 
 module.exports = {
     decode
 }
+
